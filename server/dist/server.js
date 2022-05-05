7d30069f106dc51f5851226a6a3c328de9468ff9
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-05-05 00:43:13 -0300

    Create router create Feedback

--- server/dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const teste = 1;
-async function add() {
-    return 'oie';
-}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,20 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { comment, type, screenshot } = req.body;
+  const feedback = await prisma.feedback.create({
+    data: {
+      comment,
+      type,
+      screenshot,
+    },
+  });
+  return res.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
