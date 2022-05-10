9f86dc832b16aabeed9436a8b186e73773fd8c96
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-05-10 10:15:49 -0300

    Button widget

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,7 +1,19 @@
+import { ChatTeardropDots } from 'phosphor-react-native';
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
+import { theme } from '../../theme';
 import { styles } from './styles';
 
 export function Widget() {
-  return <View></View>;
+  return (
+    <>
+      <TouchableOpacity style={styles.button}>
+        <ChatTeardropDots
+          size={24}
+          weight='bold'
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+    </>
+  );
 }
