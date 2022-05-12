01a6b7a82e82b2f0749f6f2f3f606b1c4dcdb9bc
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-05-12 15:50:08 -0300

    Change port server

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server started on port 3333');
 });
